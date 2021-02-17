53edf2187111e68a6d93ea7448b0e1b3102ddaf0
olaulvas <ola.ulvaas@gmail.com>
2021-02-17 11:33:18 +0100

    CityDao interface

---
+++ src/main/java/se/lexicon/dao/CityDao.java
@@ -0,0 +1,24 @@
+package se.lexicon.dao;
+
+import se.lexicon.model.City;
+
+import java.util.List;
+
+public interface CityDao {
+
+    City findById(int id);
+
+    List<City> findByCode(String Code);
+
+    List<City> findByName(String Name);
+
+    List<City> findAll();
+
+    City add(City city);
+
+    City update(City city);
+
+    int delete(City city);
+
+
+}
