1629e87506f83daa1f763589e890a5f37848e106
olaulvas <ola.ulvaas@gmail.com>
2021-02-17 11:18:36 +0100

    add City class

---
+++ src/main/java/se/lexicon/model/City.java
@@ -0,0 +1,48 @@
+package se.lexicon.model;
+
+import java.util.Objects;
+
+public class City {
+    private int id;
+    private String name;
+    private int code;
+
+    public City() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+
+    public int getCode() {
+        return code;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        City city = (City) o;
+        return id == city.id && code == city.code && Objects.equals(name, city.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, code);
+    }
+
+    @Override
+    public String toString() {
+        return "City{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", code=" + code +
+                '}';
+    }
+}
