a23a9699d78be27f1fa5c67b4dc0f0667ce06124
olaulvas <ola.ulvaas@gmail.com>
2021-02-17 11:41:54 +0100

    implement methods in CityDaoJDBC  - empty so far..

---
+++ src/main/java/se/lexicon/dao/CityDaoJDBC.java
@@ -0,0 +1,42 @@
+package se.lexicon.dao;
+
+import se.lexicon.model.City;
+
+import java.util.List;
+
+public class CityDaoJDBC implements CityDao{
+    @Override
+    public City findById(int id) {
+        return null;
+    }
+
+    @Override
+    public List<City> findByCode(String Code) {
+        return null;
+    }
+
+    @Override
+    public List<City> findByName(String Name) {
+        return null;
+    }
+
+    @Override
+    public List<City> findAll() {
+        return null;
+    }
+
+    @Override
+    public City add(City city) {
+        return null;
+    }
+
+    @Override
+    public City update(City city) {
+        return null;
+    }
+
+    @Override
+    public int delete(City city) {
+        return 0;
+    }
+}
